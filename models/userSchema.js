3c16fa4aeb516b64c017595f4a2961661f414f4e
Cameron <cameronkwalden@gmail.com>
2023-06-01 14:13:24 -0700

    creates user model and route

---
+++ models/userSchema.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    favoriteFilms: [String]
+});
+
+const UserModel = mongoose.model('User', userSchema);
+
+module.exports = UserModel;
