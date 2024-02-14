f802a05ab19353695dba82b393ae4a5ea6f5ea5c
Cameron <cameronkwalden@gmail.com>
2024-02-13 20:49:53 -0800

    Moves database connection to separate module

--- server.js
+++ server.js
@@ -1,29 +1,22 @@
 "use strict";
 
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const PORT = 3001;
 const verifyUser = require("./src/auth/authorize");
 const movieRouter = require("./src/routes/movies");
 const trackedRouter = require("./src/routes/tracked");
 const watchlistRouter = require("./src/routes/watchlist");
 const topFiveRouter = require("./src/routes/topFive");
+const connectDB = require("./src/db");
+
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(verifyUser);
 app.use(express.json());
 
-const mongoose = require("mongoose");
-mongoose.set("strictQuery", false);
-mongoose.connect(process.env.DB_URL);
-
-mongoose.connection.on(
-  "error",
-  console.error.bind(console, "connection error:")
-);
-mongoose.connection.once("open", () => console.log("Mongoose is connected"));
+connectDB();
 
 app.use(movieRouter)
 app.use(trackedRouter)
