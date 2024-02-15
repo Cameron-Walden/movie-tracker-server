9cfc69559df78335212f6a898c3ad6e8127e82f9
Cameron <cameronkwalden@gmail.com>
2024-02-15 13:57:00 -0800

    sets up list schema, implement POST and GET methods for /lists route

---
+++ src/routes/lists.js
@@ -0,0 +1,27 @@
+"use strict";
+
+require("dotenv").config();
+const express = require("express");
+const router = express.Router();
+const List = require("../models/listSchema");
+
+router.get("/lists", async (req, res) => {
+  try {
+    const lists = await List.find({});
+    res.status(200).send(lists);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+router.post("/lists", async (req, res, next) => {
+  try {
+    const newList = new List(req.body);
+    const result = await newList.save();
+    res.status(201).send(result);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+module.exports = router;
